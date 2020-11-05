9577aa479bdc6bfd2222c646bc5996ceb4640417
samfon24 <samfons24@yahoo.com>
2020-11-04 21:51:59 -0600

    can't get the table to be deleted and recreated with new filter

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -3,6 +3,8 @@
 // from data.js
 var tableData = data;
 
+// PART 1:
+
 // YOUR CODE HERE!
 console.log(tableData);
 
@@ -19,4 +21,49 @@ tableData.forEach(ufoEvent => {
 
     });
 
-})
+});
+
+// PART 2:
+
+var button = d3.select("#filter-btn");
+
+// When button is clicked
+button.on("click", function() {
+    var ifStatement = 1
+
+    // Select the input element and get the raw HTML node
+    var inputElement = d3.select("#datetime");
+
+    // console.log(inputElement);
+
+    // Get the value property of the input element
+    var inputValue = inputElement.property("value");
+
+    // Filter
+    var filteredData = tableData.filter(ufoEvent => ufoEvent.datetime == inputValue);
+    
+    console.log(filteredData);
+
+    if (ifStatement == 1) {
+
+        delTable = tbody.html("");
+
+    };
+
+    // Reference to table body
+    var tbody = d3.select("tbody");
+
+    // Repopulate Table with Filtered Data
+    filteredData.forEach(ufoEventFiltered => {
+
+        var row = tbody.append('tr');
+
+        Object.values(ufoEventFiltered).forEach(value => {
+
+        row.append('td').text(value);
+    
+        });
+
+    });
+    
+});
