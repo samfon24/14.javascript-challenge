4fa494d11b50eea44a8f6a1c0c6a8460ac81a348
samfon24 <samfons24@yahoo.com>
2020-11-04 20:22:51 -0600

    updated level 1 file

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,22 @@
+// LEVEL 1
+
 // from data.js
 var tableData = data;
 
 // YOUR CODE HERE!
+console.log(tableData);
+
+// Reference to table body
+var tbody = d3.select("tbody");
+
+tableData.forEach(ufoEvent => {
+
+    var row = tbody.append('tr');
+
+    Object.values(ufoEvent).forEach(value => {
+
+        row.append('td').text(value);
+
+    });
+
+})
